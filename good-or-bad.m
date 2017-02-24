34e08732acdbb8d39c664c7b734ecc75f54055cb
Shaumik-Ashraf <a.shaumik@gmail.com>
2017-02-24 14:37:52 -0500

    created player struct

--- good-or-bad.m
+++ good-or-bad.m
@@ -1,3 +1,8 @@
 %%good-or-bad
 %matlab script file
 
+player_name = input("What is your name?");
+
+Player = struct("name", player_name, "energy", 10, "inventory", [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+
