ace677d7a6122c0a9039cd45450e572822a5c676
Shaumik-Ashraf <a.shaumik@gmail.com>
2017-02-24 14:45:14 -0500

    added a item struct array and a few items

--- good-or-bad.m
+++ good-or-bad.m
@@ -1,8 +1,19 @@
 %%good-or-bad
 %matlab script file
 
-player_name = input("What is your name?");
+player_name = input('What is your name?');
 
-Player = struct("name", player_name, "energy", 10, "inventory", [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+Player = struct('name', player_name, 'energy', 10, 'inventory', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
+item(10) = struct('name', potion, 'energyEffect', 5);
+item(9)  = struct('name', poison, 'energyEffect', -5);
+item(8)  = struct('name', pebble, 'energyEffect', 0);
+%create these:
+item(7)
+item(6)
+item(5)
+item(4)
+item(3)
+item(2)
+item(1)
 
