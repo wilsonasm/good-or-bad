9a218b8cbd23ab3f60cf1de369b9588a5a16efbe
Shaumik-Ashraf <a.shaumik@gmail.com>
2017-02-24 15:42:42 -0500

    Update good-or-bad.m

--- good-or-bad.m
+++ good-or-bad.m
@@ -3,7 +3,10 @@
 
 player_name = input('What is your name?');
 
-Player = struct('name', player_name, 'energy', 10, 'inventory', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+Player = struct('name', player_name, 
+                'energy', 10, 
+                'inventory', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                'moralCounter', 0);
 
 item(10) = struct('name', potion, 'energyEffect', 5);
 item(9)  = struct('name', poison, 'energyEffect', -5);
