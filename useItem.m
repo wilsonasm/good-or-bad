524a747c8409340683f9c48da7f6eeaae06d5820
Shaumik-Ashraf <a.shaumik@gmail.com>
2017-03-03 14:38:15 -0500

    updated useItem function

--- useItem.m
+++ useItem.m
@@ -1,3 +1,12 @@
+%useItem function
+%use as follows:
+%  if( useItem(Player, 8, item_database)==false ) 
+%     player does not have item
+%  else
+%     item was used successfully
+%  end
+%
+
 function success = useItem(player, item_no, item_database)
 
   success = false;
@@ -7,6 +16,7 @@ function success = useItem(player, item_no, item_database)
       success = true;
       inv(i) = 0;
       player.effect = player.effect + item_database(item_no).energyEffect;
+      fprintf('%s used %s; %s now has %i energy.\n', player.name, item_database(item_no).name, player.name, player.effect);
     end
   end
   
