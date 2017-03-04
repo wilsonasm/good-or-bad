064dbe2edf3c78765c68924daa230635136a2d20
Shaumik Ashraf <a.shaumik@gmail.com>
2017-03-04 15:27:56 -0500

    now added first event

---
+++ first_event.m
@@ -0,0 +1,58 @@
+%first_event
+
+function [next_event_id, player] = first_event(player, item_db)
+
+    fprintf('%s, welcome to Drexel Village\n', player.name);
+    fprintf('You just graduated from the Drexel School of Chivalry\n');
+    fprintf('... at the bottom of your class.\n');
+    [~, player] = giveItem(player, 7, item_db);  %giv sword
+    [~, player] = giveItem(player, 6, item_db);  %giv food
+    fprintf('You only have a sword as your diploma and some leftovers.\n');
+    fprintf('They will be a useful tools for fending off enemies,\n');
+    fprintf('but will not fill an empty stomach forever. You need to get a job.\n');
+    fprintf('You remember your instructor told you that you can always\n');
+    fprintf('get further training as a recruit in Drexel School of Mastery\n');
+    fprintf('at Disque or Randell, or you could freelance quests at Curtis\n');
+    fprintf('You also remember your friend telling you about how the\n');
+    fprintf('underground always needs a few hands, and that you could\n');
+    fprintf('sometimes find them at the Shaft or Kelly.\n\n');
+    
+    player.energy = player.energy - 1;
+    fprintf('You spent too much time thinking, you are already losing energy\n');
+    fprintf('Just remember that your choices determine who you are.\n\n');
+    
+    fprintf('Where do you want to go?\n');
+    fprintf('Disque\n');
+    fprintf('Randell\n');
+    fprintf('Curtis\n');
+    fprintf('Shaft\n');
+    fprintf('Kelly\n');
+    
+    incorrect = true;
+    while( incorrect )
+        in = input('>>');
+        if( strcmpi(in, 'Disque') )
+            next_event_id = 10;
+            incorrect = false;
+        elseif( strcmpi(in, 'Randell') )
+            next_event_id = 20;
+            incorrect = false;
+        elseif( strcmpi(in, 'Curtis') )
+            next_event_id = 30;
+            incorrect = false;
+        elseif( strcmpi(in, 'Shaft') )
+            next_event_id = 40;
+            incorrect = false;
+        elseif( strcmpi(in, 'Kelly') )
+            next_event_id = 50;
+            incorrect = false;
+        elseif( strcmpi(in, 'Secret') )
+            next_event_id = 80;
+            incorrect = false;
+        else
+            fprintf('%s unrecognized, try again.', in);
+            incorrect = true;
+        end
+        
+    end
+end
---
+++ secret_event.m
@@ -0,0 +1,4 @@
+function [next_event_id, player] = secret_event(player, item_db)
+    
+
+end
