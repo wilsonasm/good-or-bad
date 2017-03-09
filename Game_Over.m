9f5f25e5b40738ead2d9753f293f86eaf6a3b4cf
Shaumik Ashraf <a.shaumik@gmail.com>
2017-03-09 15:05:28 -0500

    edited game_over event

--- Game_Over.m
+++ Game_Over.m
@@ -1,9 +1,10 @@
 function [next_event_id, player] = Game_Over(player, item_database)
 fprintf ('Well it looks like you finished one of the endings of the game, you have two options now\n')
-answer = input ('Restart? or End\n','s');
+answer = input ('Restart or End?\n>>','s');
     if strcmpi (answer,'Restart')
         next_event_id = 0;
     else
-        fprintf ('GG, WP')
+        fprintf ('\nGG; Well Played.\n\n');
         next_event_id = 100;
     end
+end
