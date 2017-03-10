ca7529c10924c9695f692e7c5e844cfb631c4b75
wilsonasm <markuswilson47@gmail.com>
2017-03-09 22:53:33 -0500

    quick finish

--- mountTop_event.m
+++ mountTop_event.m
@@ -1,4 +1,8 @@
 function [next_event_id, player] = mountTop_event(player, item_database)
 {
-next_event_id = 90;
+fprintf('You have reached the mountain top\n')
+fprintf('The sun is shining, the wind is blowing.\n')
+fprintf('You have survived this strange life..........\n')
+fprintf('This time\n')
+next_event_id = 200;
 }
